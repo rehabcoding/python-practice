8340ab208adf94c33c21faec05eb915057db5017
Rehab Tasawar <111608024+rehabcoding@users.noreply.github.com>
2025-04-14 18:13:13 +0000

    fourth commit

---
+++ practice/qr-generator.py
@@ -0,0 +1,19 @@
+import qrcode # imported from the qrcode library 
+
+website_link = 'https://youtu.be/T6eK-2OQtew?si=JeaGXgiafu3Tgo4i'
+
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+# `version` parameter is an integer from 1 to 40 that controls the size of the QR code
+# `box_size` parameter controls how many pixels each “box” of the QR code is
+# `border` parameter controls how many boxes thick the border should be
+
+qr.add_data(website_link)
+# giving the link to be coverted into qr
+
+qr.make()
+# to generate the qr
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+# boxes to be black, background to be white, as standard qr codes are
+
+img.save('youtube_qr.png')
